ca42a022d65d57794a0197c312685aa9c2e11540
nbloomf <nathan.bloomfield@automattic.com>
2021-06-10 23:14:07 -0500

    Update docs

--- docs/js/macros.js
+++ docs/js/macros.js
@@ -2,6 +2,19 @@ window.MathJax = {
   tex: {
     macros: {
       nats: "\\mathbb{N}",
+      LambdaIdents: "\\mathbb{I}_\\lambda",
+      LambdaConsts: "\\mathbb{C}_\\lambda",
+      LambdaTerms: "\\mathsf{Term}_\\lambda",
+      lamex: [ "\\texttt{λ} {#1}.{#2}", 2 ],
+      letin: [ "\\texttt{@let}\\ {#1}\\ \\texttt{=}\\ {#2}\\ \\texttt{@in}\\ {#3}", 3 ],
+      apply: [ "\\texttt{(}{#1}\\ {#2}\\texttt{)}", 2 ],
+      height: "\\mathsf{height}",
+      size: "\\mathsf{size}",
+      freeLambdaVars: "\\mathsf{fv}_{\\lambda}",
+      choice: "\\mathsf{choice}",
+      LambdaSubstitutions: "\\mathsf{Subst}_\\lambda",
+      new: "\\mathsf{new}",
+      AlphaEq: "\\equiv_{\\alpha}",
     }
   }
 };
